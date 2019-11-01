2922b39a41f2618b418b74678b34ee20f3132202
c4c0de <43881598+c4c0de@users.noreply.github.com>
2019-11-01 18:26:31 +0530

    new fir base app

---
+++ app/src/main/java/com/example/lastfireapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lastfireapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"firebase connection successful",Toast.LENGTH_LONG).show();
+    }
+}
